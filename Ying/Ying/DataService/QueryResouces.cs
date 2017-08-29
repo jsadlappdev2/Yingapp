950b3298d9ceab3d3a3e3527205b86057a03e9ed
jshen <jshen@anglicaresa.com.au>
2017-08-29 11:57:25 +0930

    changes.

---
+++ Ying/Ying/DataService/QueryResouces.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Ying.DataService
+{
+   public class QueryResouces
+    {
+        HttpClient client = new HttpClient();
+
+        /// <summary>
+        /// Gets the resource items async.
+        /// </summary>
+        /// <returns>The resource items async.</returns>
+          public async Task<List<ResourceItem>> GetResourceItemsAsync()
+        {
+
+            var response = await client.GetStringAsync("http://18.220.1.200/api/ying_urls/QueryAll");
+            var ResourceItem = JsonConvert.DeserializeObject<List<ResourceItem>>(response);
+            return ResourceItem;
+        }
+
+
+        /// <summary>
+        /// Gets the resource items by description key words async.
+        /// </summary>
+        /// <returns>The resource items async.</returns>
+        public async Task<List<ResourceItem>> GetResourceItemByDescsAsync(string desc)
+        {
+            string url = "http://localhost:60761/api/ying_urls/Querybydesc?desc=" + desc;
+
+
+            var response = await client.GetStringAsync(url);
+            var ResourceItem = JsonConvert.DeserializeObject<List<ResourceItem>>(response);
+            return ResourceItem;
+        }
+
+
+        public class ResourceItem
+        {
+            public int id { get; set; }
+            public string type { get; set; }
+            public string url { get; set; }
+            public string description { get; set; }
+            public DateTime entrytime { get; set; }
+            public string valid_flag { get; set; }
+
+            public string icon { get; set; }
+
+        }
+
+
+
+    }
+}
--- Ying/Ying/Views/Resource.xaml.cs
+++ Ying/Ying/Views/Resource.xaml.cs
@@ -6,15 +6,48 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Ying.DataService;
 
 namespace Ying.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Resource : ContentPage
     {
+        DataService.QueryResouces queryresources;
+
+        List<DataService.QueryResouces.ResourceItem> items;
+
+
         public Resource()
         {
             InitializeComponent();
+            queryresources = new DataService.QueryResouces();
         }
+
+        async void RefreshData()
+        {
+            items = await queryresources.GetResourceItemsAsync();
+            resourceList.ItemsSource = items.OrderBy(item => item.type).ThenBy(item => item.id).ToList();
+        }
+
+
+        async void searchresource()
+        {
+            items = await queryresources.GetResourceItemByDescsAsync(searchentry.Text.Trim());
+            resourceList.ItemsSource = items.OrderBy(item => item.type).ThenBy(item => item.id).ToList();
+        }
+
+
+        public async void onplay(object sender, EventArgs e)
+        {
+            //var mi = ((MenuItem)sender);
+            //TodoItem itemToDelete = (TodoItem)mi.CommandParameter;
+            ////int itemIndex = items.IndexOf(itemToDelete);
+            //int itemIndex = itemToDelete.id;
+
+            //await dataService.DeleteTodoItemAsync(itemIndex);
+            RefreshData();
+        }
+
     }
 }
